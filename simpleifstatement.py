6b2c3d20af8e225372cf63aff67e75107ccd1733
Pieter Fourie <pfourie507@gmail.com>
2025-11-01 11:55:26 +0200

    Small file change

--- simpleifstatement.py
+++ simpleifstatement.py
@@ -13,14 +13,4 @@ name = 'Faasen'
 
 if name.lower() == 'faasen':
     print("Hello, Faasen! Welcome back.")
-
-# Example of checking age for alcohol purchase    
-
-alcohol_age = 18  # Set the age limit for alcohol purchase
-
-age = int(input("Enter your age: "))  # Check age
-
-if age >= alcohol_age:
-    print("You are old enough to buy alcohol.")
-else:
-    print("You are not old enough to buy alcohol.")
+    
